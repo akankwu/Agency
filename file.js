bcaa1cffaaa287407b495e943e6ad80aee110e4b
akankwu <75956188+akankwu@users.noreply.github.com>
2023-10-17 05:28:51 -0700

    Add files via upload

--- file.js
+++ file.js
@@ -1,7 +1,7 @@
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -15,4 +15,14 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+}
+
+// Add this JavaScript to toggle the sidebar
+function toggleSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  if (sidebar.style.right === '0px') {
+    sidebar.style.right = '-250px'; // Hide the sidebar
+  } else {
+    sidebar.style.right = '0px'; // Show the sidebar
+  }
+}
